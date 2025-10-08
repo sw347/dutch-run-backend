248dccb9238a73241059101dce3a7ac899984dbb
sw347 <sdh230310@sdh.hs.kr>
2025-10-08 14:28:57 +0200

    chore: url 추가

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from game.views import GameCreateView, GameLeaderboardViewSet, GameDeleteView, DailySeedView
+from game.views import GameCreateView, GameLeaderboardViewSet, GameDeleteView, DailySeedView, GameTopLankView
 
 router = DefaultRouter()
 router.register(r'games', GameLeaderboardViewSet, basename='game')
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('game/create/', GameCreateView.as_view(), name='game-create'),
     path('game/delete/<int:pk>/', GameDeleteView.as_view(), name='game-delete'),
+    path('game/top-rank/', GameTopLankView, name='game-top-rank'),
     path('api/daily_seed/', DailySeedView.as_view(), name='daily-seed'),
     path('', include(router.urls)),
 ]
